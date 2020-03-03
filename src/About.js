9d23d87c1c8f80d93e2256975d1f6be1a8662b0a
github username <github email address>
2020-03-02 22:01:03 -0500

    button

--- src/About.js
+++ src/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { Button } from 'reactstrap';
 
 export const About = (props) => {
 
     return (
       <div>
               <h1>Jordan Simon</h1>
-
+              <Button outline color="secondary" size="lg" block>View Resume</Button>
 
       </div>
     )
