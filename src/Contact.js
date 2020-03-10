7fef231d2d5c1d801017b0bc867af09fe8def114
fkayoungjo <Jordan.simon55@gmail.com>
2020-03-09 23:38:33 -0400

    eod3

--- src/Contact.js
+++ src/Contact.js
@@ -12,10 +12,10 @@ export const Contact = (props) => {
       <Container className = "divs">
       <h1 className= "name" >CONTACT</h1>
 
-      <a href ="https://www.linkedin.com/in/jordansimondeveloper/" target="_blank"><img src={linkedin} alt="linkedin" width="40" height="40"/></a>
-      <a href ="https://github.com/fkayoungjo" target="_blank"><img src={git} alt="git" width="40" height="40"/></a>
-      <a href ="https://medium.com/@jordan.simon55" target="_blank"><img src={medium} alt="medium" width="40" height="40"/></a>
-      <a href ="mailto:jordan.simon55@gmail.com?subject=Email from Website" ><img src={email} alt="email" width="40" height="50"/></a>
+      <a href ="https://www.linkedin.com/in/jordansimondeveloper/" target="_blank" className="links"><img src={linkedin} alt="linkedin" width="40" height="40"/></a>
+      <a href ="https://github.com/fkayoungjo" target="_blank" className="links"><img src={git} alt="git" width="40" height="40"/></a>
+      <a href ="https://medium.com/@jordan.simon55" target="_blank" className="links"><img src={medium} alt="medium" width="40" height="40"/></a>
+      <a href ="mailto:jordan.simon55@gmail.com?subject=Email from Website" className="links"><img src={email} alt="email" width="40" height="50"/></a>
       </Container>
       </div>
     )
