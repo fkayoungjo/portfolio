4c538663977fb2f1be973cbe9503838c48a2e1f4
fkayoungjo <Jordan.simon55@gmail.com>
2020-09-17 13:13:01 -0400

    change

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -15,7 +15,7 @@ export const Portfolio = (props) => {
                 <CardImg top width="100%" src={hungry} alt="hungrynow" />
                 <CardBody>
                   <CardTitle className= "cardtext">Hungry Now</CardTitle>
-                  <CardSubtitle className= "cardtext">Recipe/Restaurant finder web application (React.js, Redux, PostgreSQL, Rails, Reactstrap) </CardSubtitle>
+                  <CardSubtitle className= "cardtext">Recipe/Restaurant finder web application (React.js, Redux, Rails, Reactstrap) </CardSubtitle>
                   <CardText><a href ="https://youtu.be/-jqgZddr4WI">Demo</a><br></br>
                   <a href ="https://github.com/fkayoungjo/HungryNow">Front End</a><br></br>
                   <a href =" https://github.com/fkayoungjo/HungryNow/tree/master/hungry-now-2-backend">Back End</a></CardText>
